7e7722eb53b44a77ed29051139d3d4634ec4aa4f
Kamdoum <kamdoumloich@yahoo.fr>
2022-02-21 22:47:21 +0100

    Dice app finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         appBar: AppBar(
           title: Text('Deux Dee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.green[900],
         ),
         body: DicePage(),
       ),
@@ -15,15 +17,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left_dice = 1;
+  int right_dice = 1;
+
+  void changeNumber() {
+    setState(() {
+      left_dice = 1 + Random().nextInt(6);
+      right_dice = 1 + Random().nextInt(6);
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [],
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeNumber();
+              },
+              child: Image.asset('images/dice$left_dice.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeNumber();
+              },
+              child: Image.asset('images/dice$right_dice.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
